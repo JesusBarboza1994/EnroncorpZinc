23e2e4608edd244f918a8b43806e4cd06155fff2
Jesus Barboza <jbarz20@gmail.com>
2023-05-21 21:19:31 -0500

    ADD: set the routers for the api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JesusBarboza1994/EnroncorpZinc
 
 go 1.20
+
+require github.com/go-chi/chi v1.5.4 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-
+	"log"
+	"fmt"
+	"net/http"
 	"github.com/JesusBarboza1994/EnroncorpZinc/config"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 
 
 func main() {
-	config.UpZinc()
-	config.LoopFoldersOfAUser()
+	r := chi.NewRouter()
+
+	// Agrega el middleware de recuperación de errores
+	r.Use(middleware.Recoverer)
+
+	if !config.IndexExist() {
+		fmt.Println("Index created")
+		config.UpZinc()
+		config.LoopUsers()
+	}
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+
+	
 }
